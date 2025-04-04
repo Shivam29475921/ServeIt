ff1c5eaf0ecc587164f5d5ee9461863a4e3b7364
Shivam Das <shivamdas16122002@gmail.com>
2025-04-04 23:04:30 +0530

    Update settings.py

--- a_core/settings.py
+++ a_core/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-rj#-z^kx3j+1ay397otg6j8m_8#v^$^$jys6&41vy^&6le)ezc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
+ALLOWED_HOSTS = ['https://web-production-eb03c.up.railway.app/', 'localhost', '127.0.0.1', '*']
 
 CSRF_TRUSTED_ORIGINS = ['https://*']
 
